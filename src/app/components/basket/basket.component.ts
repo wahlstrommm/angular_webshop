190f729ce33100b0ea73838a2bf1c300bf4746c9
Magnus Vahlström <Wahlstrommm@gmail.com>
2022-03-10 16:37:16 +0100

    First commit

---
+++ src/app/components/basket/basket.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { BasketService } from 'src/app/services/basket.service';
+
+@Component({
+  selector: 'app-basket',
+  templateUrl: './basket.component.html',
+  styleUrls: ['./basket.component.scss'],
+})
+export class BasketComponent implements OnInit {
+  constructor(private basketService: BasketService) {}
+  itemsToBasket: any;
+  itemsInfoToBasket:any;
+  toogleContainer: boolean = false;
+  toogleDisplay() {
+    this.toogleContainer = !this.toogleContainer;
+  }
+  ngOnInit(): void {
+    this.basketService.activeOrder.subscribe(
+      (itemsToBasket) => (this.itemsToBasket = itemsToBasket)
+    );
+    this.itemsArrayFromUser=this.itemsToBasket
+
+    
+  }
+  
+  itemsArrayFromUser: any;
+
+  shield: string = '/assets/shield.svg';
+}
